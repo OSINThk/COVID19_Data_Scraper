b1d0dd65f1820e57ef13eee4c27b18c4b51aa72a
Syed Shariq Ali <syed-aly@github.com>
2020-04-08 17:12:59 +0500

    Adding api for globbal stats

--- Wikipedia_Scraper/WikipediaService.py
+++ Wikipedia_Scraper/WikipediaService.py
@@ -10,8 +10,8 @@ class WikipediaService(object):
 
     def get_global_stats(self):
         print(f"Fetching global stats.")
-        self.scraper = Scraper(url="https://en.m.wikipedia.org/wiki/2019%E2%80%9320_coronavirus_pandemic")
-        res = self.process_table("Template:2019–20 coronavirus pandemic data", "2", "-2", "2", "3", "4", "5")
+        self.scraper = Scraper(url="https://en.m.wikipedia.org/wiki/2019%E2%80%9320_coronavirus_pandemic_by_country_and_territory")
+        res = self.process_table("Countries and territories", "2", "-2", "2", "3", "4", "5")
         return res
 
     def search_table(self, text):
