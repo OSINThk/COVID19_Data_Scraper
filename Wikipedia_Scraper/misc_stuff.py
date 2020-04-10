238fe37482fc539eded61833446c10df0a37fc84
Syed Shariq Ali <syed-aly@github.com>
2020-04-10 22:53:12 +0500

    Revert "worldometer data scraper"
    This reverts commit 04444b66

--- Wikipedia_Scraper/misc_stuff.py
+++ Wikipedia_Scraper/misc_stuff.py
@@ -2,7 +2,7 @@ import datetime
 import json
 import os
 import time
-
+import pendulum
 import pandas as pd
 import csv
 import requests
@@ -12,12 +12,14 @@ import numpy as np
 from GeoJsonService import GeoJsonService
 
 columns = ["country", "region", "infected", "deaths", "recoveries", "long", "lat", "last_updated"]
-other_output_csv = "output/static_output.csv"
+hopkins_csv = "output/john_hopkins_output.csv"
+taiwan_csv = "output/taiwan.csv"
+japan_csv = "output/japan.csv"
 
 
 def write_record_to_output(record, filename=None):
     if filename is None:
-        filename = other_output_csv
+        filename = hopkins_csv
     fields = record.keys()
     parent_dir_path = os.path.dirname(os.path.realpath(__file__))
     filepath = os.path.join(parent_dir_path, filename)
@@ -91,6 +93,7 @@ def scrape_taiwan_data():
                      "&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Confirmed"
                      "%20desc&resultOffset=0&resultRecordCount=25&cacheHint=true")
     response = r.json()
+    cleanup(taiwan_csv)
     for feature in response["features"]:
         attributes = feature["attributes"]
         city = attributes["COUNTYENG"]
@@ -99,8 +102,7 @@ def scrape_taiwan_data():
         deaths = attributes["death"]
         latitude, longitude = g.get_lat_long(country, city)
         write_to_file(country=country, region=city, infected=infected, recoveries=recovered, deaths=deaths,
-                      long=longitude, lat=latitude, filename="output/Taiwan.csv")
-        print(f'"{country}","{city}","{latitude}","{longitude}"')
+                      long=longitude, lat=latitude, filename=taiwan_csv)
 
 
 def scrape_china_data(date_str=None):
@@ -136,6 +138,7 @@ def scrape_japan_data():
     r = requests.get('https://data.covid19japan.com/summary/latest.json')
     response = r.json()
     prefectures = response["prefectures"]
+    cleanup(japan_csv)
     for prefecture in prefectures:
         region = prefecture["name"]
         confirmed = prefecture["confirmed"]
@@ -143,24 +146,36 @@ def scrape_japan_data():
         recoveries = prefecture["recovered"]
         latitude, longitude = g.get_lat_long(country, region)
         write_to_file(country=country, region=region, infected=confirmed, deaths=deaths, recoveries=recoveries,
-                      long=longitude, lat=latitude)
+                      long=longitude, lat=latitude, filename=japan_csv)
 
 
 def scrape_world_data(date_str=None):
     excluded_countries = ["China"]
-    if date_str is None:
-        date_str = datetime.datetime.now().strftime("%m-%d-%Y")
+    countries = ["Australia", "Canada", "China", "US"]
+    curr_date = pendulum.now()
+    processed = False
+    if not date_str:
+        date_str = curr_date.strftime("%m-%d-%Y")
+    while not processed:
         print(date_str)
-        date_str = "04-09-2020"
-    r = requests.get("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data"
-                     f"/csse_covid_19_daily_reports/{date_str}.csv")
+        print(f"Trying {date_str}...")
+        r = requests.get("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data"
+                         f"/csse_covid_19_daily_reports/{date_str}.csv")
+        if r.status_code == 200:
+            processed = True
+        else:
+            print(f"Error! Status Code: {r.status_code}")
+            curr_date = curr_date.subtract(days=1)
+            date_str = curr_date.strftime("%m-%d-%Y")
+
     if r.status_code == 200:
         req = StringIO(r.text)
         df = pd.read_csv(req).replace(np.nan, '', regex=True)
-        print(df[:5])
+        df = df[df["Country_Region"].isin(countries)]
     else:
         print(r)
         return
+    cleanup(hopkins_csv)
     for index, row in df.iterrows():
         country = row["Country_Region"]
         if country in excluded_countries:
@@ -173,8 +188,10 @@ def scrape_world_data(date_str=None):
         active = row["Active"]
         latitude = row["Lat"]
         longitude = row["Long_"]
+        if "Unassigned" in region:
+            continue
         write_to_file(country=country, region=region, infected=confirmed, deaths=deaths, recoveries=recoveries,
-                      long=longitude, lat=latitude)
+                      long=longitude, lat=latitude, filename=hopkins_csv)
     return df
 
 
@@ -202,24 +219,15 @@ def calculate_sum_and_write(df):
                       long=longitude, lat=latitude)
 
 
-def cleanup():
+def cleanup(filename=hopkins_csv):
     parent_dir_path = os.path.dirname(os.path.realpath(__file__))
-    filepath = os.path.join(parent_dir_path, other_output_csv)
+    filepath = os.path.join(parent_dir_path, filename)
     if os.path.exists(filepath):
         os.remove(filepath)
 
 
-def worldometer():
-    r = requests.get('https://www.worldometers.info/coronavirus/#countries')
-
-
 if __name__ == '__main__':
     last_updated = datetime.datetime.now()
-    # cleanup()
-    # world_df = scrape_world_data()
-    # calculate_sum_and_write(world_df)
-    # scrape_japan_data()
+    world_df = scrape_world_data()
+    scrape_japan_data()
     scrape_taiwan_data()
-    # print("Phillipines")
-    # scrape_philipines_data()
-    # print("World")
