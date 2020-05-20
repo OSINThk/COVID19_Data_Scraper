c19ed365fdff3991e304146e75633b5b21a19079
Syed Shariq Ali <syed-aly@github.com>
2020-05-20 11:32:29 +0500

    updated worldometer data format

--- Wikipedia_Scraper/WorldoMeterService.py
+++ Wikipedia_Scraper/WorldoMeterService.py
@@ -14,7 +14,7 @@ class WorldoMeterService(object):
         self.scraper = Scraper(
             url="https://www.worldometers.info/coronavirus/#countries")
         table = self.search_table_by_id('main_table_countries_today')
-        res = self.process_table(table, "9", "-8", "1", "2", "4", "6", "7", "9", "10", "11", "3", "5")
+        res = self.process_table(table, "9", "-8", "2", "3", "5", "7", "8", "10", "11", "12", "4", "6", "14")
         return res
 
     def get_us_stats(self):
@@ -73,7 +73,7 @@ class WorldoMeterService(object):
 
     def process_table(self, table, start_row, end_row, country_col, total_col, death_col, recovered_col,
                       active_col, total_per_mil_col, deaths_per_mil_col, total_tests_col, new_cases_col,
-                      new_deaths_col):
+                      new_deaths_col, population_col):
         if start_row.isnumeric():
             table = table[int(start_row) - 1:]
         if end_row.lstrip('-+').isnumeric():
@@ -81,57 +81,65 @@ class WorldoMeterService(object):
         result = []
         region = ""
         for row in table:
-            country = row[int(country_col) - 1]
-            total = deceased = recovered = active = total_per_mil = total_tests = deaths_per_mil = "0"
-            active_per_mil = recovered_per_mil = tests_per_mil = new_cases = new_deaths = "0"
-            if total_col.isnumeric():
-                total = row[int(total_col) - 1]
-                total = total if total.strip() else "0"
-            if active_col.isnumeric():
-                active = row[int(active_col) - 1]
-                active = active if active.strip() else "0"
-            if deaths_per_mil_col.isnumeric():
-                deaths_per_mil = row[int(deaths_per_mil_col) - 1]
-                deaths_per_mil = deaths_per_mil if deaths_per_mil.strip() else "0"
-            if total_per_mil_col.isnumeric():
-                total_per_mil = row[int(total_per_mil_col) - 1]
-                total_per_mil = total_per_mil if total_per_mil.strip() else "0"
-            if total_tests_col.isnumeric():
-                total_tests = row[int(total_tests_col) - 1]
-                total_tests = total_tests if total_tests.strip() else "0"
-            if death_col.isnumeric():
-                deceased = row[int(death_col) - 1]
-                deceased = deceased if deceased.strip() else "0"
-            if recovered_col.isnumeric():
-                recovered = row[int(recovered_col) - 1]
-                recovered = recovered if recovered.strip() else "0"
-            if total_per_mil and total_per_mil != "0":
-                pop = int(sanitize_digit(total)) / int(sanitize_digit(total_per_mil))
-                active_per_mil = int(int(sanitize_digit(active)) / pop)
-                recovered_per_mil = int(int(sanitize_digit(recovered)) / pop)
-                tests_per_mil = int(int(sanitize_digit(total_tests)) / pop)
-            if new_cases_col.isnumeric():
-                new_cases = row[int(new_cases_col) - 1]
-                new_cases = sanitize_digit(new_cases) if new_cases.strip() else "0"
-            if new_deaths_col.isnumeric():
-                new_deaths = row[int(new_deaths_col) - 1]
-                new_deaths = sanitize_digit(new_deaths) if new_deaths.strip() else "0"
-            d = dict(country=country,
-                     region=region,
-                     infected=total,
-                     deaths=deceased,
-                     recoveries=recovered,
-                     active=active,
-                     total_per_mil=total_per_mil,
-                     deaths_per_mil=deaths_per_mil,
-                     total_tests=total_tests,
-                     active_per_mil=active_per_mil,
-                     recovered_per_mil=recovered_per_mil,
-                     tests_per_mil=tests_per_mil,
-                     new_cases=new_cases,
-                     new_deaths=new_deaths,
-                     last_updated=self.last_updated.strftime("%Y-%m-%d %H:%M:%S"))
-            result.append(d)
+            try:
+                country = row[int(country_col) - 1]
+                total = deceased = recovered = active = total_per_mil = total_tests = deaths_per_mil = "0"
+                active_per_mil = recovered_per_mil = tests_per_mil = new_cases = new_deaths = pop = "0"
+                if total_col.isnumeric():
+                    total = row[int(total_col) - 1]
+                    total = total if total.strip() else "0"
+                if active_col.isnumeric():
+                    active = row[int(active_col) - 1]
+                    active = active if active.strip() else "0"
+                if deaths_per_mil_col.isnumeric():
+                    deaths_per_mil = row[int(deaths_per_mil_col) - 1]
+                    deaths_per_mil = deaths_per_mil if deaths_per_mil.strip() else "0"
+                if total_per_mil_col.isnumeric():
+                    total_per_mil = row[int(total_per_mil_col) - 1]
+                    total_per_mil = total_per_mil if total_per_mil.strip() else "0"
+                if total_tests_col.isnumeric():
+                    total_tests = row[int(total_tests_col) - 1]
+                    total_tests = total_tests if total_tests.strip() else "0"
+                if death_col.isnumeric():
+                    deceased = row[int(death_col) - 1]
+                    deceased = deceased if deceased.strip() else "0"
+                if recovered_col.isnumeric():
+                    recovered = row[int(recovered_col) - 1]
+                    recovered = recovered if recovered.strip() else "0"
+                if population_col.isnumeric():
+                    pop = row[int(population_col) - 1]
+                    pop = pop if pop.strip() else "0"
+                if pop != "0":
+                    population_per_mil = int(sanitize_digit(pop)) / 1000000
+                    if population_per_mil>0:
+                        active_per_mil = int(int(sanitize_digit(active)) / population_per_mil)
+                        recovered_per_mil = int(int(sanitize_digit(recovered)) / population_per_mil)
+                        tests_per_mil = int(int(sanitize_digit(total_tests)) / population_per_mil)
+                if new_cases_col.isnumeric():
+                    new_cases = row[int(new_cases_col) - 1]
+                    new_cases = sanitize_digit(new_cases) if new_cases.strip() else "0"
+                if new_deaths_col.isnumeric():
+                    new_deaths = row[int(new_deaths_col) - 1]
+                    new_deaths = sanitize_digit(new_deaths) if new_deaths.strip() else "0"
+                d = dict(country=country,
+                         region=region,
+                         infected=total,
+                         deaths=deceased,
+                         recoveries=recovered,
+                         active=active,
+                         total_per_mil=total_per_mil,
+                         deaths_per_mil=deaths_per_mil,
+                         total_tests=total_tests,
+                         active_per_mil=active_per_mil,
+                         recovered_per_mil=recovered_per_mil,
+                         tests_per_mil=tests_per_mil,
+                         new_cases=new_cases,
+                         new_deaths=new_deaths,
+                         pop=pop,
+                         last_updated=self.last_updated.strftime("%Y-%m-%d %H:%M:%S"))
+                result.append(d)
+            except Exception as e:
+                print(row, e)
         return result
 
 
